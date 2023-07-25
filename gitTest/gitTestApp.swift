797b8152e2ea97031659d7555bac75407e017f38
xavier flaherty <xflaherty@mybce.catholic.edu.au>
2023-07-25 14:20:52 +1000

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Xavier Flaherty on 25/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
